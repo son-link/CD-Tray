8064736651c1adc39d9ec5df6d290eafe3ed5de4
Alfonso Saavedra Son <sonlink.dourden@gmail.com>
2012-09-20 17:41:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,13 @@ mkdir -p /usr/share/cdtray
 install -m644 COPYING /usr/share/cdtray
 sed -i "s/lang/\/usr\/share\/locale/g" /usr/bin/cdtray
 sed -i "s/COPYING/\/usr\/share\/cdtray\/COPYING/g" /usr/bin/cdtray
+echo 'You use Debian/Ubuntu/Linux Mint on other deribated distributions? [y/N]'
+read debian
+if [ $debian = 'y' ] || [ $debian = 'Y' ]; then
+	patch -Np1 -i cdtray_debian.patch /usr/bin/cdtray
+else
+	patch -Np1 -i cdtray.patch /usr/bin/cdtray
+fi
 cp lang/* -r /usr/share/locale
 
-echo 'Istall complete'
+echo 'Install complete'
